5b90be0974986e3af26b99431f6a19a78fa41959
nataliarosa88 <42919658+nataliarosa88@users.noreply.github.com>
2019-03-05 16:32:00 -0300

    nova classe service

---
+++ br/com/andretecnologia/makeup/test/ServiceTest.java
@@ -0,0 +1,23 @@
+package br.com.andretecnologia.makeup.test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.andretecnologia.makeup.model.Service;
+
+public class ServiceTest {
+public static void main(String[] args) {
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("makeup");
+	EntityManager em = emf.createEntityManager();
+	
+	Service service1 = new Service();
+	service1.setDescription("makeup");
+	
+	em.getTransaction().begin();		
+	em.persist(service1);
+	em.getTransaction().commit();
+	em.close();
+}
+}
